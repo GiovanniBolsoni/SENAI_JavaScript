bcd84d1e42dbcaeb6cfd2bdb63ca9ab112eeaeda
GiovanniBolsoni <giovannibolsoni502@gmail.com>
2026-05-19 17:43:33 -0300

    feat: atividade inicial de sintaxe js

---
+++ lanchonete_brasaburguer/script.js
@@ -0,0 +1,20 @@
+const shopName = "Brasa Burguer";
+console.log(`===== ${shopName} =====`);
+
+let cliente;
+let produto;
+let precoUnitario;
+let quantidade;
+let totalCompra;
+
+cliente = prompt("Digite seu nome:");
+console.log(`Cliente: ${cliente}`);
+
+produto = prompt("Digite seu pedido (X-Bacon, X-Salada e X-Tudo): ");
+console.log(`Produto: ${produto}`);
+
+if(produto == "X-Bacon"){
+    precoUnitario
+}
+
+// precoUnitario = produto == "X-bacon" ? 20 : produto == "X-salada" ? 10 : 80; 
