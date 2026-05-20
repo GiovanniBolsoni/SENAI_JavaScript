f687b68134747d4295963729884bb73147e9f5cc
Giovanni Bolsoni <giovannibolsoni502@gmail.com>
2026-05-19 23:44:28 -0300

    feat: atividade de sistema de pedido de lanche js

--- lanchonete_brasaburguer/script.js
+++ lanchonete_brasaburguer/script.js
@@ -1,20 +1,40 @@
-const shopName = "Brasa Burguer";
-console.log(`===== ${shopName} =====`);
-
+// variaveis
 let cliente;
 let produto;
 let precoUnitario;
 let quantidade;
 let totalCompra;
 
+// nome da lanchonete
+const shopName = "Brasa Burguer";
+console.log(`===== ${shopName} =====`);
+
+// nome do cliente
 cliente = prompt("Digite seu nome:");
 console.log(`Cliente: ${cliente}`);
 
+// nomes dos produtos
 produto = prompt("Digite seu pedido (X-Bacon, X-Salada e X-Tudo): ");
 console.log(`Produto: ${produto}`);
 
-if(produto == "X-Bacon"){
-    precoUnitario
+// preços dos produtos
+if(produto == "X-Salada"){
+    precoUnitario = 20;
+    console.log(`Valor: R$${precoUnitario}`);
+} else if(produto == "X-Bacon"){
+    precoUnitario = 25;
+    console.log(`Valor: R$${precoUnitario}`);
+} else if(produto == "X-Tudo"){
+    precoUnitario = 30;
+    console.log(`Valor: R$${precoUnitario}`);
 }
 
-// precoUnitario = produto == "X-bacon" ? 20 : produto == "X-salada" ? 10 : 80; 
+// quantidade de pedidos
+quantidade = Number(prompt("Digite a quantidade desejada: "));
+
+// valor total do pedido
+totalCompra = precoUnitario * quantidade;
+console.log (`Total da compra: R$${totalCompra}`);
+
+// mesagem final do pedido
+console.log("Pedido realizado com sucesso. Obrigado pela preferência!");
