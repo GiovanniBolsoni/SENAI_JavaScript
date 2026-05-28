c18029993047e1ea23efbc38ffaaefd54680f91e
GiovanniBolsoni <giovannibolsoni502@gmail.com>
2026-05-28 15:17:01 -0300

    atualização dos exercícios de condicionais

---
+++ condicionais/lanchonete_brasaburguer/script.js
@@ -0,0 +1,40 @@
+// variaveis
+let cliente;
+let produto;
+let precoUnitario;
+let quantidade;
+let totalCompra;
+
+// nome da lanchonete
+const shopName = "Brasa Burguer";
+console.log(`===== ${shopName} =====`);
+
+// nome do cliente
+cliente = prompt("Digite seu nome:");
+console.log(`Cliente: ${cliente}`);
+
+// nomes dos produtos
+produto = prompt("Digite seu pedido (X-Bacon, X-Salada e X-Tudo): ");
+console.log(`Produto: ${produto}`);
+
+// preços dos produtos
+if(produto == "X-Salada"){
+    precoUnitario = 20;
+    console.log(`Valor: R$${precoUnitario}`);
+} else if(produto == "X-Bacon"){
+    precoUnitario = 25;
+    console.log(`Valor: R$${precoUnitario}`);
+} else if(produto == "X-Tudo"){
+    precoUnitario = 30;
+    console.log(`Valor: R$${precoUnitario}`);
+}
+
+// quantidade de pedidos
+quantidade = Number(prompt("Digite a quantidade desejada: "));
+
+// valor total do pedido
+totalCompra = precoUnitario * quantidade;
+console.log (`Total da compra: R$${totalCompra}`);
+
+// mesagem final do pedido
+console.log("Pedido realizado com sucesso. Obrigado pela preferência!");
