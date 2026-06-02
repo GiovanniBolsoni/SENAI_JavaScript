734b86c1106a04ebb608099f4aae4816e469c3f2
GiovanniBolsoni <giovannibolsoni502@gmail.com>
2026-06-02 17:19:45 -0300

    atualização dos exercícios de localStorage

--- LocalStorage/atividade_Lanchonete/scripts/script.js
+++ LocalStorage/atividade_Lanchonete/scripts/script.js
@@ -1,54 +1,36 @@
-const inputPedido = document.getElementById("input-pedido");
-const botaoAdicionar = document.getElementById("btn-adicionar");
-const listaPedidos = document.getElementById("lista-pedidos");
-const botaoAtender = document.getElementById("btn-atender");
-const pedidoAtual = document.getElementById("pedido-atual");
+const inputPedido = document.getElementById("inputPedido");
+const botaoAdicionar = document.getElementById("btnAdicionar");
+const listaPedidos = document.getElementById("listaPedidos");
+const botaoAtender = document.getElementById("btnAtender");
+const pedidoAtual = document.getElementById("pedidoAtual");
 
-botaoAdicionar.addEventListener("click", () => {
-    let pedidoDigitado = inputPedido.value;
+let pedidos = [];
 
-    if (pedidoDigitado === "") {
-        return;
-    }
+// Carregando dados do localStorage
+const dados = localStorage.getItem("pedidos");
 
-    const li = document.createElement("li");
+// Se existir dados no localStorage
+if(dados) {
+    //a lista de pedidos recebe os dados convertidos em JSON
+    pedidos = JSON.parse(dados);
 
-    li.innerText = pedidoDigitado;
+    //! CHAMAR A FUNÇÕA DE RENDERIZAR/mostrar na tela
+}
 
-    listaPedidos.appendChild(li);
+function salvar() {
+    // A lista é convertida em texto e armezenada dentro do local do localStorage
+    localStorage.setItem("pedidos", JSON.stringify(pedidos));
+}
 
-    inputPedido.value = "";
+function renderizar() {
+    listaPedidos.innerHTML = "";
 
-    const botaoRemover = document.createElement("button")
-    botaoRemover.innerText = "🗑️";
-    li.appendChild(botaoRemover);
+    pedidos.forEach((pedidos, posicao) => {
+        const li = document.createElement("li");
+        li.innerText = pedidoAtual.texto;
 
-    botaoRemover.style.backgroundColor = "#e65c00";
-    botaoRemover.style.border = "none";
-
-    botaoRemover.addEventListener("click", () => {
-        li.remove();
-    });
-
-    li.addEventListener("click", () => {
-        if(li.style.textDecoration === "line-through") {
-            li.style.textDecoration = "none";
-        } else {
-            li.style.textDecoration = "line-through"
+        if(pedido.concluido) {
+            li.style.textDecoration = "line-through";
         }
-    });
-});
-
-botaoAtender.addEventListener("click", () => {
-    const primeiroPedido = listaPedidos.firstChild
-
-    pedidoAtual.innerText = "Atendendo: " + primeiroPedido.innerText;
-
-    primeiroPedido.remove();
-});
-
-inputPedido.addEventListener("keydown", (dados) => {
-    if (dados.key === "Enter") {
-        botaoAdicionar.click();
-    }
-});
+    })
+}
